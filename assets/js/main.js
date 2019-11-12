39028d624b655c9fe6a49765a9d520600d494c80
Artur Bruno <arturbrunoferreira@gmail.com>
2019-11-12 15:57:28 -0300

    codigo identado

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var json = require('../data/cidades.json')
 function percorrerArray() {
     json.cidades.forEach(e => {
         console.log(e.id)
-        if(e.id <= 10) {
+        if (e.id <= 10) {
             console.log('value maior que 10')
         } else {
             console.log('value menor que 10')
