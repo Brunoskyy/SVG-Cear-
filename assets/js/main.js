81fcaae7bf126cc0412f3ca30a52ed9af1c186d6
Artur Bruno <arturbrunoferreira@gmail.com>
2019-11-12 15:53:34 -0300

    inserida funcao que percorre o Array

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,11 @@ var json = require('../data/cidades.json')
 function percorrerArray() {
     json.cidades.forEach(e => {
         console.log(e.id)
-        console.log(e.nome)
+        if(e.id <= 10) {
+            console.log('value maior que 10')
+        } else {
+            console.log('value menor que 10')
+        }
         console.log(e.value)
     });
 }
